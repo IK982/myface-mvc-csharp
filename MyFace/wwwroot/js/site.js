117ba7206bfa2e7ccf456ed8c8b3ab696a905951
India <india.kaurbrown@zoopla.co.uk>
2020-07-29 13:04:46 +0100

    js initial task
    when you click on "posts", the posts delete

---
+++ MyFace/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+    //your code
+    let header = document.getElementById("posts-section");
+    
+    header.addEventListener("click", function(){ header.style.display="none" });
+
+    // header.style.color = "red";
+    // header.style.backgroundColor = "red";
+   });
+   
